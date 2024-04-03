c8dda6740460f0d52bfc1b4b6fd50abba6b0cb3f
Ben Jarrett <ben.jarrett@insight.com>
2024-04-03 09:27:15 -0400

    a few more notes

--- src/src/server.ts
+++ src/src/server.ts
@@ -2,6 +2,7 @@ import http from 'http';
 import express from 'express';
 import './config/logging';
 import { loggingHandler } from './middleware/loggingHandler';
+import { corsHandler } from './middleware/corsHandler';
 
 export const application = express();
 export let httpServer: ReturnType<typeof http.createServer>;
@@ -19,4 +20,25 @@ export const Main = () => {
     logging.info('Logging and Configuration');
     logging.info('-----------------------------------------------');
     application.use(loggingHandler);
+    application.use(corsHandler);
+
+    // Defining our Controller Routing //
+    logging.info('-----------------------------------------------');
+    logging.info('Define Controller Routing');
+    logging.info('-----------------------------------------------');
+    // just adding a health check for now //
+    // adding some json on a successful request to check that our API is running correctly from a browser or a tool like Postman //
+    application.get('/main/healthcheck', (req, res, next) => {
+        return res.status(200).json({ hello: 'world' });
+    });
+
+    // Defining our Error Routing //
+    logging.info('-----------------------------------------------');
+    logging.info('Define Error Routing');
+    logging.info('-----------------------------------------------');
+    // creating an error route //
+    // adding some json on a successful request to check that our API is running correctly from a browser or a tool like Postman //
+    application.get('/main/healthcheck', (req, res, next) => {
+        return res.status(200).json({ hello: 'world' });
+    });
 };
